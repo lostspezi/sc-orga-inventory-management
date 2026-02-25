15019a6e759a5c6b08d12cb0fc1007ae5d0fa269
LostSpezi <marcell.dechant@proton.me>
2026-02-25 15:29:23 +0100

    chore: Add custom 404 Not Found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+        <>
+            NOT FOUND!
+            <br/>
+            <Link href="/">Go Home</Link></>
+    );
+}
